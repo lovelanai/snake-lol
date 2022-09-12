10af754549886acdede27aeae59426bced386bf2
Love Lanai <love.lanai@medieinstitutet.se>
2022-09-12 16:01:38 +0200

    joner broner now  got mover

--- src/App.js
+++ src/App.js
@@ -1,13 +1,80 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useRef, useEffect, useState } from "react";
+import apple from "./assets/pngegg.png";
 
 function App() {
+  const [snakePositionY, setSnakePositionY] = useState(10);
+  const [snakePositionX, setSnakePositionX] = useState(10);
+  const [snakeRotation, setSnakeRotation] = useState(0);
+
+  const handleKeyDown = (event) => {
+    if (event.keyCode === 38) {
+      up();
+    }
+    if (event.keyCode === 40) {
+      down();
+    }
+
+    if (event.keyCode === 39) {
+      right();
+    }
+
+    if (event.keyCode === 37) {
+      left();
+    }
+  };
+
+  const up = () => {
+    setSnakePositionY(snakePositionY + 20);
+    console.log(snakePositionY);
+    setSnakeRotation(270);
+  };
+
+  const down = () => {
+    setSnakePositionY(snakePositionY - 20);
+    console.log(snakePositionY);
+    setSnakeRotation(-270);
+  };
+
+  const left = () => {
+    setSnakePositionX(snakePositionX - 20);
+    console.log(snakePositionX);
+    setSnakeRotation(180);
+  };
+
+  const right = () => {
+    setSnakePositionX(snakePositionX + 20);
+    console.log(snakePositionX);
+    setSnakeRotation(0);
+  };
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    ref.current.focus();
+  }, []);
+
   return (
     <div className="gameContainer">
-      <h1>Jånas the snake</h1>
+      <h1 className="name">
+        Jånas the <br></br>parkour god snake
+      </h1>
       <div className="gameContent">
-        <div className="snakeArea">
-          <div className="jonerbroner">
+        <div
+          style={{
+            bottom: `${snakePositionY}px`,
+            left: `${snakePositionX}px`,
+            rotate: `${snakeRotation}deg`,
+          }}
+          className="snakeArea"
+        >
+          <div
+            className="jonerbroner"
+            ref={ref}
+            tabIndex={-1}
+            onKeyDown={handleKeyDown}
+          >
             <div className="face">
               <div className="eyes">
                 <div className="left-eye"></div>
@@ -16,6 +83,16 @@ function App() {
               <div className="mun"></div>
             </div>
           </div>
+        </div>
+
+        <div
+          style={{
+            height: "3rem",
+            position: "absolute",
+          }}
+          className="apple"
+        >
+          <img src={apple}></img>
         </div>
       </div>
     </div>
