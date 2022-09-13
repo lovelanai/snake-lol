810de713bce3c06d519257d667cc38475fd82616
Love Lanai <love.lanai@medieinstitutet.se>
2022-09-13 09:43:55 +0200

    snake can now poop

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useRef, useEffect, useState } from "react";
 import apple from "./assets/pngegg.png";
+import poopHouse from "./assets/poophouse.png";
 
 function App() {
   const [snakePositionY, setSnakePositionY] = useState(0);
   const [snakePositionX, setSnakePositionX] = useState(0);
   const [snakeRotation, setSnakeRotation] = useState(0);
   const [snakeWidth, setSnakeWidth] = useState(8);
+  const [snakeHeight, setSnakeHeight] = useState(2);
   const [appleHit, setAppleHit] = useState(false);
   const [applePositionX, setApplePositionX] = useState(100);
   const [applePositionY, setApplePositionY] = useState(100);
   const [isSnakeFat, setIsSnakeFat] = useState(false);
+  const [faceColor, setFaceColor] = useState("rgb(155, 10, 168)");
+  const [bodyColor, setBodyColor] = useState("rgb(155, 10, 168)");
+  const [pooped, setPooped] = useState(false);
 
   const handleKeyDown = (event) => {
     if (event.keyCode === 38) {
@@ -69,13 +74,43 @@ function App() {
     if (snakePositionY < -100) {
       setSnakePositionY(700);
     }
+
+    if (snakeWidth > 10) {
+      setIsSnakeFat(true);
+    }
+
+    if (!isSnakeFat) {
+      setFaceColor("rgb(155, 10, 168)");
+      setBodyColor("rgb(155, 10, 168)");
+      setSnakeHeight(2);
+    }
+
+    if (isSnakeFat) {
+      setFaceColor("red");
+      setBodyColor("red");
+      setSnakeHeight(4);
+    }
+
+    if (isSnakeFat && snakePositionY >= 500 && snakePositionX <= 60) {
+      setPooped(true);
+      setSnakeWidth(8);
+      console.log("jag sket");
+    }
+
+    if (pooped & (snakeWidth < 10)) {
+      setIsSnakeFat(false);
+    }
+
     collision();
+
+    console.log(snakePositionX + "X");
+    console.log(snakePositionY + "y");
   }, [snakePositionX, snakePositionY]);
 
-  const collision = () => {
-    // let x = snakePositionX === applePositionX;
-    // let y = snakePositionY === applePositionY;
+  let poopHouseX = "0";
+  let poopHouseY = "0";
 
+  const collision = () => {
     if (
       (snakePositionX === applePositionX) &
       (snakePositionY === applePositionY)
@@ -83,23 +118,18 @@ function App() {
       setSnakeWidth(snakeWidth + 3);
       setAppleHit(true);
 
-      let max = 1000 / 20 + 1;
       let resultX = parseInt(Math.random() * 30) * 20;
       let resultY = parseInt(Math.random() * 30) * 20;
 
-      //randomMultiple = random - (random % 20);
-
       setApplePositionX(resultX);
       setApplePositionY(resultY);
-
-      console.log("fetare");
     }
     renderAnotherApple();
+    /* if */
   };
 
   const renderAnotherApple = () => {
     setAppleHit(false);
-    /* collision(); */
   };
 
   return (
@@ -108,6 +138,22 @@ function App() {
         Jånas the <br></br>parkour god snake
       </h1>
       <div className="gameContent">
+        {isSnakeFat ? (
+          <>
+            <div
+              style={{ top: `${poopHouseY}rem`, left: `${poopHouseX}rem` }}
+              className="poopHouse"
+            >
+              <img src={poopHouse}></img>
+            </div>
+            <div className="full">
+              <h1>SNAKE IS FULL</h1>
+              <p>USE TOILET!</p>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
         <div className="snakeArea">
           <div
             className="hitbox"
@@ -122,9 +168,14 @@ function App() {
               ref={ref}
               tabIndex={-1}
               onKeyDown={handleKeyDown}
-              style={{ width: `${snakeWidth}rem` }}
+              style={{
+                width: `${snakeWidth}rem`,
+                height: `${snakeHeight}rem`,
+                background: `${bodyColor}`,
+              }}
             >
-              <div className="face">
+              <div style={{ background: `${faceColor}` }} className="face">
+                {isSnakeFat ? <div className="svett"></div> : <></>}
                 <div className="eyes">
                   <div className="left-eye"></div>
                   <div className="right-eye"></div>
